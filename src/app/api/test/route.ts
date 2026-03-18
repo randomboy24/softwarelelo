7b1bbf59473b0197be63ce99e695772a36571fbf
randomboy24 <jatinthegod21@gmail.com>
2026-03-18 16:19:51 +0530

    add test route to check prisma  connection

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  const users = await prisma.user.findMany();
+  return NextResponse.json({
+    users: users,
+  });
+};
